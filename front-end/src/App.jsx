c589fef6df76261a0a5756421ac9549f20c6d0c3
Abdullah <abdullah.habib@arbisoft.com>
2023-10-09 15:19:19 +0500

    .

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import {routes, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <>
+
+    <Navbar />
       
-    </div>
+    </>
   );
 }
 

995d988cf005c22f603ccef17ecfb8a6e4f7436d
Abdullah <abdullah.habib@arbisoft.com>
2023-10-12 13:56:16 +0500

    css

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import axios from 'axios'
 import {toast} from 'react-hot-toast'
 import { useNavigate } from "react-router-dom"
+import '../styles/login.css'
 
 const Login = () => {
   const navigate = useNavigate()
@@ -34,16 +35,18 @@ const Login = () => {
     }
   }
   return (
-    <div>
-      <form onSubmit={loginUser}>
-      <label>Email</label>
-        <input type="email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+    <div className="container">
+      <div className="form-container">
+        <form onSubmit={loginUser}>
+          <label>Email</label>
+          <input type="email" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
 
-        <label>Password</label>
-        <input type="password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+          <label>Password</label>
+          <input type="password" value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
 
-        <button type='submit'>Login</button>
-      </form>
+          <button type='submit'>Login</button>
+        </form>
+      </div>
     </div>
   )
 }
--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
 import { useNavigate } from "react-router-dom"
+import '../styles/register.css'
 
 const Register = () => {
 
@@ -40,19 +41,21 @@ const Register = () => {
 
   }
   return (
-    <div>
-      <form onSubmit={registerUser}>
-        <label>Name</label>
-        <input type="text" value={data.name} onChange={(e) => setData({...data, name: e.target.value})} />
+    <div className="container">
+      <div className="form-container">
+        <form onSubmit={registerUser}>
+          <label>Name</label>
+          <input type="text" value={data.name} onChange={(e) => setData({ ...data, name: e.target.value })} />
 
-        <label>Email</label>
-        <input type="email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+          <label>Email</label>
+          <input type="email" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
 
-        <label>Password</label>
-        <input type="password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+          <label>Password</label>
+          <input type="password" value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
 
-        <button type='submit'>Submit</button>
-      </form>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
     </div>
   )
 }

fa38f89b473a5f4e440634ca9ce957650eef6b4c
Abdullah <abdullah.habib@arbisoft.com>
2023-10-13 16:52:21 +0500

    okay

--- front-end/src/pages/Dashboard.jsx
+++ front-end/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+/*import { useContext } from "react";
 import { UserContext } from "../context/userContext";
 
 export default function Dashboard() {
@@ -14,5 +14,27 @@ export default function Dashboard() {
       )}
     </div>
   );
+}*/
+
+import { useContext } from "react";
+import { UserContext } from "../context/userContext";
+import '../styles/dashboard.css'; // 
+
+export default function Dashboard() {
+  const { user } = useContext(UserContext);
+
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-box">
+        <h1>Welcome!</h1>
+        {user ? (
+          <h1>{user.name}, Best Javascript Developer of Arbisoft</h1>
+        ) : (
+          <p>Loading user data...</p>
+        )}
+      </div>
+    </div>
+  );
 }
+
 

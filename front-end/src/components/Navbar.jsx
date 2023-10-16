fd3035e780e4a345f99d71f43b2459fd4969c51d
Abdullah <abdullah.habib@arbisoft.com>
2023-10-16 15:52:56 +0500

    .

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -10,12 +10,18 @@ const Navbar = () => {
         <Link to='/'>Home</Link>
         <Link to='/register'>Register</Link>
         <Link to='/login'>Login</Link>
+
+
       </div>
     </nav>
   );
 }
 
 export default Navbar;
+
+
+
+
 
 
 

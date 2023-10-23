98cdc0680b8974afff04d3b26d0d96062455a6b9
Abdullah <abdullah.habib@arbisoft.com>
2023-10-23 12:32:33 +0500

    Added just a comment

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { test, registerUser, loginUser, getProfile } = require('../controllers/authController')
 
 // middleware hai for CORS
-//important
+//important Hello
 
 router.use(
     cors({
